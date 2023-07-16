cd44031d138f1af6ac1815aaf364a2abc33cbfa4
Preameswar <83682282+Preameswar@users.noreply.github.com>
2023-07-16 14:18:03 +0530

    Add files via upload

---
+++ Contollers/HeaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Popupdesign.Feature.Header.Contollers
+{
+    public class HeaderController : Controller
+    {
+        // GET: Header
+        public ActionResult Index()
+        {
+            return View("/Views/Popupdesign/Feature/Header.cshtml");
+        }
+    }
+}
